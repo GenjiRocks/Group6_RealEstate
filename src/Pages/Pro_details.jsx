3de08c6b5a59f08715c902c3cbac7ad049fbf136
Ashwin <ashwindeepak1997@gmail.com>
2024-07-02 21:35:09 +0530

    july2

---
+++ src/Pages/Pro_details.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pro_details() {
+  return (
+    <div>Pro_details</div>
+  )
+}
+
+export default Pro_details
---
+++ src/Pages/Pro_listing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pro_listing() {
+  return (
+    <div>Pro_listing</div>
+  )
+}
+
+export default Pro_listing
