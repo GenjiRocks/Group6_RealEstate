ab5805a88e3477ded616c26c9ed8117e9454822f
abhishekabhii96 <abhishekgnair96@gmail.com>
2024-07-13 15:13:25 +0530

    text font changed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,24 +4,24 @@ import Header from '../Components/Header'
 function Home() {
   return (
     <>
-    <Header/>
-    <div className="container">
-    <div className="row w-100 p-4">
-    <div className='col-md-9'>
-      <h2>Best Properties For Sale In Kerala</h2>
-      <span className='heading-divider'></span>
-      <p className='mb-6'>
-        "Here is the list of properties for sale in kerala,that include apartments,flats,office spaces,and houses for sale in kerala."
-      </p>
-    </div>
+      <Header />
+      <div className="container">
+        <div className="row w-100 p-4">
+          <div className='col-md-9'>
+            <h2>Best Properties For Sale In Kerala</h2>
+            <span className='heading-divider'></span>
+            <p className='mb-6 text-decoration: none'>
+              "Here is the list of properties for sale in kerala,that include apartments,flats,office spaces,and houses for sale in kerala."
+            </p>
+          </div>
 
-    <div className='col-md-3'></div>
-   
-    </div>
-    </div>
-    <div>
-      Home</div>
-      </>
+          <div className='col-md-3'></div>
+
+        </div>
+      </div>
+      <div>
+        Home</div>
+    </>
   )
 }
 
