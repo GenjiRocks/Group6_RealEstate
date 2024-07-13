70220939d7d980d4beb033c7096f75e9f6a57ed6
abhishekabhii96 <abhishekgnair96@gmail.com>
2024-07-13 15:34:46 +0530

    edited

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
           <div className='col-md-9'>
             <h2>Best Properties For Sale In Kerala</h2>
             <span className='heading-divider'></span>
-            <p className='mb-6 text-decoration: none'>
+            <p className='mb-6 '>
               "Here is the list of properties for sale in kerala,that include apartments,flats,office spaces,and houses for sale in kerala."
             </p>
           </div>
