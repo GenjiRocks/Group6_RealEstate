35da460cb2c6428ec04ea2175b210d7ac2da96d8
abhishekabhii96 <abhishekgnair96@gmail.com>
2024-07-13 12:28:12 +0530

    updated home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className="container">
+    <div className="row w-100 p-4">
+    <div className='col-md-9'>
+      <h2>Best Properties For Sale In Kerala</h2>
+      <span className='heading-divider'></span>
+      <p className='mb-6'>
+        "Here is the list of properties for sale in kerala,that include apartments,flats,office spaces,and houses for sale in kerala."
+      </p>
+    </div>
+
+    <div className='col-md-3'></div>
+   
+    </div>
+    </div>
+    
+    </>
   )
 }
 
